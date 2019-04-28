fac82ff99219cfe6f992d9134d27324516b2cab3
Richard Bertozzo <richardjrbertozzo@gmail.com>
2019-04-28 20:04:57 -0300

    Setup project quarkus

---
+++ src/main/java/richardbertozzo/Hello.java
@@ -0,0 +1,16 @@
+package richardbertozzo;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class Hello {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/test/java/richardbertozzo/NativequarkusIT.java
@@ -0,0 +1,9 @@
+package richardbertozzo;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativequarkusIT extends quarkusTest {
+
+    // Execute the same tests but in native mode.
+}
